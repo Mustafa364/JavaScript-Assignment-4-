b410399607e630ee3d37c61c67133f7f7a9525db
Ghulam Mustafa <116076196+Mustafa364@users.noreply.github.com>
2022-11-16 02:01:09 +0500

    Add files via upload

---
+++ JavaScript Assingment # 04/app.js
@@ -0,0 +1,155 @@
+
+            // CHAPTER # 01 ( ALERTS ) 
+
+        // alert("Error! Please enter a valid password.");
+
+        // alert('Welcome to JS Land... \nHappy Coding!'); 
+
+            // alert("Welcome to JS Land..."); 
+            // alert('Happy Coding! \nPrevent this page from creating additonal dialogs'); 
+            // alert("Hello... I can run JS through my web browser's console.");
+
+            // CHAPTER # 02 ( VARIABLES FOR STRINGS )
+            
+            // var myname;
+            // myname = ("Ghulam Mustafa");
+            // alert(myname);
+
+            // var myage;
+            // myage = ("19 years old");
+            // alert(myage);
+
+            // var mycourse;
+            // mycourse = ("Certified Mobile Application Development");
+            // alert(mycourse);
+
+            // var pizza;
+            // pizza = ('PIZZA \nPIZZ \nPIZ \nPI \nP');
+            // alert(pizza);
+
+            // var email;
+            // email = ("dellLaptop@email.com");
+            // alert(email);
+
+            // var AsmarterWayToLearnJavaScript;
+            // AsmarterWayToLearnJavaScript = ('I am trying to learn from the Book A Smarter \n way to learn JavaScript');
+            // alert(AsmarterWayToLearnJavaScript);
+
+            // var yah = (" Yah! I can write HTML content through JavaScript");
+            // document.write(yah);
+
+            // alert('\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2297\xA5\u2297\u2014\u2014\u2014\u2014\u2014\u2014\u2014\u2014')
+
+                    // CHAPTER # 03 ( VARIABLES FOR NUMBERS  )
+
+                // var age;
+                // age = ("I am 19 years old");
+                // alert(age);
+
+                // var web;
+                // web = ("you have visited this site 15 times");
+                // alert(web);
+
+                // var birth;
+                // birth = ('My birth year is 2003\n Data type of my declared variable is number');
+                // alert(birth);
+
+                // document.write("John Doe ordered 5 T-shirts on XYZ Clothing store");
+
+
+                // CHAPTER # 04 ( VARIABLE NAMES: LEGAL & ILLEGAL   )
+
+                // var abc;
+                // abc = ('\xA7\u2014\u20141stVariable');
+                
+                // var jkl;
+                // jkl = ('\xA7name,\u2014 or name');
+                
+                // // var 12mnmDD;
+                // var fool;
+                // var foo2;
+                // document.write('Rules for naming JS variables \n');
+                
+
+                // CHAPTER # 05 ( MATH EXPRESSIONS   )
+
+                // var num;
+                // num = (3+5);
+                // document.write(num);
+                // document.write("<br>");
+                // var num;
+                // num = (8-5);
+                // document.write(num);
+                // document.write("<br>");
+                // var num;
+                // num = (3*5);
+                // document.write(num);
+                // document.write("<br>");
+                // var num;
+                // num = (9/5);
+                // document.write(num);
+
+                // var num =(5);
+                // num = (5 + 1);
+                // num = (num + 7);
+                // num = (num - 1);
+                // num = ("The remainder is : 0");
+                // document.write(num);
+
+                // var movie = (600);
+                // movie = (600*5);
+                // document.write(movie);
+
+                // var totalmarks = ( 800);
+                // var obtainedmarks = (530);
+                // var percentage = (totalmarks/obtainedmarks);
+                // document.write(percentage);
+
+                    // CHAPTER # 12-13 ( IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS   )
+                    
+                    // var time = 10;
+                    // if (time <= 10) {
+                    //     alert("Good morning");
+                    //   }
+                    //    else {
+                    //     alert("Good evening");
+                    //   };
+
+
+                    // CHAPTER # 14-16 ( ARRAYS )
+
+                    // var pak; 
+                    // arr1 = ["SSC","HSC","BCS","BC","BCOM","MS","MPhil.","PhD"];
+                    // document.write(pak);
+
+                    // pak = ['Ahmed is 320 percentage 64% \n Raza is 230 percentage 46%\n Hasan is 480 percentage 96%'];
+                    // document.write(pak);
+
+
+                    // CHAPTER # 17-20 ( ARRAYS AND LOOP )
+
+         
+                    // for(var input = 1; input <= 10; input++ ){document.write(input);}
+
+                    // var number = parseInt(prompt('Enter an Number: '));
+                    // for(var i = 1; i <= 10; i++) {
+                    //     var result = i * number;
+                    //     document.write(`${number} * ${i} = ${result}`);}
+
+
+                                // CHAPTER # 09-11 ( USER INPUT & CONDITIONAL STATEMENT )
+
+                               
+                                // var city = "karachi";
+                                // var city1 = prompt("enter the city name");
+                                // if(city == city1){
+                                //     document.write("Welcome to city of lights");
+                                // }else{document.write("this is also a great city of pakistan");}
+
+
+                                // var gender = "male";
+                                // var gender1 = prompt("enter the gender");
+                                // if(gender == gender1){
+                                //     document.write("Good Morning Sir");
+                                // }else{document.write("Good Morning Ma'am");}
+
